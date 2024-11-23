a5466d8136abd11b0ec3c0cb0461a57b8a0de05c
aman212 <aman.motghare2024@gmail.com>
2024-11-24 03:47:21 +0530

    correct cors

--- frontend/src/components/NoteItem.js
+++ frontend/src/components/NoteItem.js
@@ -1,68 +1,3 @@
-// import React from "react";
-// import NoteContext from "../context/notes/NoteContext";
-// import { useContext } from "react";
-
-// const NoteItem = (props) => {
-//   const context = useContext(NoteContext);
-//   const { deleteNote, togglePin } = context; // Include togglePin function
-//   const { note, updateNote } = props;
-
-//   return (
-//     <>
-//       <div className="col-md-4">
-//         <div className="card my-3">
-//           <div className="card-body">
-//             <div className="d-flex align-items-center">
-//               <h5 className="card-title">{note.title}</h5>
-//               <div className="ms-auto">
-//                 {/* Pin/Unpin Icon */}
-//                 <i
-//                   className={`fa-solid ${
-//                     note.pinned ? "fa-thumbtack" : "fa-thumbtack-slash"
-//                   } mx-2`}
-//                   onClick={() => {
-//                     togglePin(note._id); // Call togglePin function
-//                   }}
-//                   style={{
-//                     cursor: "pointer",
-//                     color: note.pinned ? "gold" : "gray",
-//                   }}
-//                   title={note.pinned ? "Unpin Note" : "Pin Note"}
-//                 ></i>
-
-//                 {/* Edit Note Icon */}
-//                 <i
-//                   className="fa-regular fa-pen-to-square mx-2"
-//                   onClick={() => {
-//                     updateNote(note);
-//                   }}
-//                   style={{ cursor: "pointer" }}
-//                 ></i>
-
-//                 {/* Delete Note Icon */}
-//                 <i
-//                   className="fa-regular fa-trash-can mx-2"
-//                   onClick={() => {
-//                     deleteNote(note._id);
-//                     props.showAlert("Note deleted successfully", "success");
-//                   }}
-//                   style={{ cursor: "pointer" }}
-//                 ></i>
-//               </div>
-//             </div>
-//             <p className="card-text">{note.description}</p>
-//             <p className="card-text">{note.tag}</p>
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default NoteItem;
-
-
-
 import React from "react";
 import NoteContext from "../context/notes/NoteContext";
 import { useContext } from "react";
@@ -77,7 +12,7 @@ const NoteItem = (props) => {
   return (
     <>
       <div className="col-md-4">
-        <div className="card my-3">
+        <div className="card my-3 shadow" style={{background:"rgb(252 252 252)"}}>
           <div className="card-body">
             <div className="d-flex align-items-center">
               <h5 className="card-title">{note.title}</h5>
